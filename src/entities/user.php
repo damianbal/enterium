a3a4343497e1dc03a35b65019ba612beb28db70a
Damian <balandowski@icloud.com>
2018-03-18 18:07:48 +0000

    User entity update
    removed some code which was just to test the entity

--- src/entities/user.php
+++ src/entities/user.php
@@ -8,18 +8,6 @@ class User extends Entity
 {
     protected static $table = 'users';
 
-    public function wypisz()
-    {
-        return "To jest " . $this->username . " ma haslo " . $this->password . " :D";
-    }
-
-    public function czyJestAdminem()
-    {
-        if($this->password == 'damian1') return true;
-
-        return false;
-    }
-
     protected static $props = [
         'username' => 'varchar(255)',
         'password' => 'varchar(300)',
